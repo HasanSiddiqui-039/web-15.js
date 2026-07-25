98ccf9acbf5ac73496f972cd0a6aa32f150d825e
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-07-25 00:26:13 -0700

    first commit

---
+++ index1.js
@@ -0,0 +1,35 @@
+var name = "Hasan";
+console.log(name);
+
+let language = "English";
+console.log(language);
+
+const website = "Google";
+console.log(website);
+
+var age = 20;
+console.log(age);
+
+let students = 45;
+console.log(students);
+
+const days = 7;
+console.log(days);
+
+var isStudent = true;
+console.log(isStudent);
+
+let hasLicense = true;
+console.log(hasLicense);
+
+const isOpen = true;
+console.log(isOpen);
+
+var price = 99.99;
+console.log(price);
+
+let temperature = 36.7;
+console.log(temperature);
+
+const pi = 3.14159;
+console.log(pi);
