625ace2f0c1c0330c03d82585ba8fc4e767ecfff
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-08-06 18:45:48 +0500

    Add functions for various operations in index4.js

---
+++ index4.js
@@ -0,0 +1,29 @@
+let name = function(Hasan){
+   return Hasan;
+}
+console.log(name("Hasan"));
+
+let num=function(a,b){
+    return a+b;
+}
+console.log(num(5,6));
+
+let name1 = function(Usman,Musab){
+return Usman + Musab;
+}
+console.log(name1("Usman","Musab"));
+
+let number=function(b,c){
+    return b*c;
+}
+console.log(number(2,4));
+
+let name2= (Hassan) => 
+    { return Hassan;
+}
+console.log(name2("Hassan"));
+
+let num1= (a,b) => 
+    { return a-b;
+}
+console.log(num1(10,5));
