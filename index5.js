4a72de39346c2737c16c3c689e935c995b709c53
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-08-11 18:42:36 +0500

    Update index5.js

--- index5.js
+++ index5.js
@@ -6,3 +6,19 @@ for (let i = 1; i <= 10; i++) {
 for ( i = 1; i <= 10; i++) {
     console.log(`5 x ${i} = ${5 * i}`);
 }
+
+var name1 = function(Usman,Musab){
+return Usman + Musab;
+}
+console.log(name1("Usman","Musab"));
+
+let num1= (a,b) => 
+    { return a-b;
+}
+console.log(num1(10,5));
+
+function name3(Hassan){
+    return Hassan;
+}
+
+console.log(name3("Hassan"));
