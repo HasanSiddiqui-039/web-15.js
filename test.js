ce64c0beedef1d15ab2124e1ca8854fea16a01ab
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-08-15 18:49:29 +0500

    Add various JavaScript examples and console logs

---
+++ test.js
@@ -0,0 +1,59 @@
+let array = ["Apple","Banana","Mango"];
+console.log(array);
+
+
+let  JavaScript= function() 
+  { console.log("welcome Student");
+}
+JavaScript();
+
+
+let r=1;
+for(r =1; r<= 5;) {
+    console.log(r);
+    ++r;
+}
+
+let q=1;
+while (q <= 5) {
+    console.log(q);
+    ++q;
+} 
+
+let p = 1;
+do {
+    console.log(p);
+    ++p;
+} while (p <= 5);
+
+let fruit = ["Apple","Banana","Mango"];
+fruit.shift();
+console.log(fruit);
+
+var fruit1 = ["Apple","Banana","Mango"];
+fruit1.unshift("Grapes");
+console.log(fruit1);
+
+let fruit2 = ["Apple","Banana","Mango"];
+fruit2.splice(1,0,"Grapes");
+console.log(fruit2);
+
+let fruit3 = ["Apple","Banana","Mango"];
+fruit3.slice(1,1, "pineapple");
+console.log(fruit3);
+
+let fruit4 = ["Apple","Banana","Mango"];
+fruit4.pop( "Apple");
+console.log(fruit4);
+
+let fruit5 = ["Apple","Banana","Mango"];
+fruit5.push( "Apple");
+console.log(fruit5);
+
+for ( i = 1; i <= 10; i++) {
+    console.log(`2 x ${i} = ${2 * i}`);
+}
+
+let name = "Hasan";
+if(true);
+console.log(name);
