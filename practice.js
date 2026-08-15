dbf0063b9c3da56f906226c76b7c4f96c9ce1c86
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-08-15 00:49:53 -0700

    Add practice.js with fruit array and greeting function

---
+++ practice.js
@@ -0,0 +1,11 @@
+let fruits = ["Apple", "Mango", "Banana", "Orange"];
+console.log(fruits);
+
+function greet() {
+    console.log("Hello Hasan!");
+}
+greet();
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
