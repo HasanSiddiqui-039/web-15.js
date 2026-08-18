abb6296897d1cb2b66f6d3107a53c1ef037f2b00
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-08-18 18:50:16 +0500

    Create Day7.js

---
+++ Day7.js
@@ -0,0 +1,41 @@
+let a = 5;
+let b = 8;
+console.log(a + b);
+
+let c = 9;
+let d = 4;
+console.log(c - d);
+
+let e = 5;
+let f = 5;
+console.log(e * f);
+
+let g = 2;
+let h = 4;
+console.log(g / h);
+
+let x = 10;
+x += 5; 
+console.log(x);
+
+let q = 8;
+q -= 3;  
+console.log(q);
+
+let r = 9;
+r *= 2; 
+console.log(r); 
+
+let u = 6;
+u /= 4;  
+console.log(u);
+
+let v = 4;
+let s = 8;
+ console.log(v != s);
+ console.log(v == s);
+ console.log(v > s);
+ console.log(v < s);
+ console.log(v === s);
+ console.log(v <= s);
+ console.log(v >= s);
