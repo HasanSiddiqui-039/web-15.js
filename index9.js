f69975c399d2f889a534c2ca81707735af68c2fd
HasanSiddiqui-039 <hs0721507@gmail.com>
2026-09-02 11:00:56 -0700

    Refactor number checks and user prompts in index9.js

--- index9.js
+++ index9.js
@@ -4,3 +4,67 @@ if(num %2 == 0){
 }else{
     console.log("Your number is odd")
 }
+
+let num = Number(prompt("Enter your number"))
+if(num <= 1){
+    console.log("Not a prime number")
+}else if(num % 2 == 0 && num != 2){
+    console.log("Not a prime number")
+}else{
+    console.log("Prime number")
+}
+
+let num = Number(prompt("Enter a number"))
+if(num > 0){
+    console.log("Your number is positive")
+}else{
+    console.log("Your number is negative")
+}
+
+let marks = Number(prompt("Enter your marks"))
+if(marks >= 40){
+    console.log("You are pass")
+}else{
+    console.log("You are fail")
+}
+
+let age = Number(prompt("Enter your age"))
+if(age >= 18){
+    console.log("You can vote")
+}else{
+    console.log("You cannot vote")
+}
+
+let num = Number(prompt("Enter your number"))
+if(num % 5 == 0){
+    console.log("Your number is divisible by 5")
+}else{
+    console.log("Your number is not divisible by 5")
+}
+
+let num = Number(prompt("Enter your number"))
+if(num == 0){
+    console.log("Your number is zero")
+}else if(num % 2 == 0){
+    console.log("Your number is even")
+}else{
+    console.log("Your number is odd")
+}
+
+let name = prompt("Enter your name")
+
+if(name == "Hasan" || name == "Ahmed"){
+    console.log("Welcome")
+}else{
+    console.log("Access denied")
+}
+
+let num = Number(prompt("Enter your number"))
+
+if(num <= 1){
+    console.log("Not a prime number")
+}else if(num % 2 == 0 && num != 2){
+    console.log("Not a prime number")
+}else{
+    console.log("Prime number")
+}
